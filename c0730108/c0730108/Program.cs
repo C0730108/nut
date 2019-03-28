24b0687a0463c4b542aca35cb54282c052a4b7e6
Jashandeep  Kingra <730108@student.tor.loc>
2019-03-28 16:21:27 -0400

    commit

--- c0730108/c0730108/Program.cs
+++ c0730108/c0730108/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace c0730108
@@ -11,6 +12,16 @@ namespace c0730108
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world");
+            Download();
+            Console.ReadLine();
+
         }
+        static void Download()
+        {
+            //task 1
+            Thread.Sleep(7000);
+            Console.WriteLine("Download complete");
+        } 
+        
     }
 }
